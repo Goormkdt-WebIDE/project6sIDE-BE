06f6eccfd5a7d756337ffb01200a95197ea8d121
Shingyu Kang <rkdtlseb@naver.com>
2023-09-11 15:39:21 +0900

    legend starts

---
+++ src/test/java/com/project5s/IDEproject/IdEprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project5s.IDEproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdEprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
