f3a7bc7766a9e257d5dec508f8cb6b3fa24dd698
Shingyu Kang <rkdtlseb@naver.com>
2023-09-12 18:46:38 +0900

    [feat] add deploy test controller

---
+++ src/main/java/com/project5s/IDEproject/controller/DeployTestController.java
@@ -0,0 +1,12 @@
+package com.project5s.IDEproject.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DeployTestController {
+    @GetMapping("deployTest")
+    public String deployTest(){
+        return "Success deploy";
+    }
+}
