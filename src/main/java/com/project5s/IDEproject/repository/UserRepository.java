d0520ec773f02b5c77924882f11ca05f0e370c67
soonhankwon <soonable@gmail.com>
2023-09-15 20:23:51 +0900

    refactor : User renamed & 애노테이션 추가(#6)

--- src/main/java/com/project5s/IDEproject/repository/UserRepository.java
+++ src/main/java/com/project5s/IDEproject/repository/UserRepository.java
@@ -1,10 +1,12 @@
 package com.project5s.IDEproject.repository;
 
-import com.project5s.IDEproject.domain.Users;
+import com.project5s.IDEproject.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
-public interface UserRepository  extends JpaRepository<Users, Long> {
-    Optional<Users> findByUserName(String userName);
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
 }
