c881cae9e10ba1f18bc09b043652915babe4ad63
soonhankwon <soonable@gmail.com>
2023-09-15 20:34:02 +0900

    refactor : record 사용으로 리팩토링(#6)

--- src/main/java/com/project5s/IDEproject/controller/UserController.java
+++ src/main/java/com/project5s/IDEproject/controller/UserController.java
@@ -21,21 +21,21 @@ public class UserController {
     @Operation(summary = "회원가입 API")
     @PostMapping("/join")
     public ResponseEntity<String> join(@RequestBody UserJoinRequest dto) {
-        userService.join(dto.getUsername(), dto.getPassword());
+        userService.join(dto.username(), dto.password());
         return ResponseEntity.ok().body("회원가입이 완료되었습니다.");
     }
 
     @Operation(summary = "로그인 API & ACCESS_TOKEN 발급")
     @PostMapping("/login")
     public ResponseEntity<String> log(@RequestBody UserLoginRequest dto) {
-        String token = userService.login(dto.getUsername(), dto.getPassword());
+        String token = userService.login(dto.username(), dto.password());
         return ResponseEntity.ok().body(token);
     }
 
     @Operation(summary = "비밀번호 리셋 API")
     @PostMapping("/reset")
     public ResponseEntity<String> reset(@RequestBody UserResetRequest dto) {
-        userService.resetPassword(dto.getUsername(), dto.getPassword(), dto.getNewPassword());
+        userService.resetPassword(dto.username(), dto.password(), dto.newPassword());
         return ResponseEntity.ok().body("비밀번호가 변경되었습니다.");
     }
 }
