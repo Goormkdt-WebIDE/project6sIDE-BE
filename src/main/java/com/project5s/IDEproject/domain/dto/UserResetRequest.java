d46ddbb3381d674163d0e6c6685eb3df3395ecb9
soonhankwon <soonable@gmail.com>
2023-09-15 20:23:24 +0900

    refactor : DTO record 로 변경(#6)

--- src/main/java/com/project5s/IDEproject/domain/dto/UserResetRequest.java
+++ src/main/java/com/project5s/IDEproject/domain/dto/UserResetRequest.java
@@ -1,12 +1,4 @@
 package com.project5s.IDEproject.domain.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-
-@AllArgsConstructor
-@Getter
-public class UserResetRequest {
-    private String userName;
-    private String password;
-    private String newPassword;
+public record UserResetRequest(String username, String password, String newPassword) {
 }
