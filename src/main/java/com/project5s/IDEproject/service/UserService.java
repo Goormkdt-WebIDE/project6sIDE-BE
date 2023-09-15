0f89fee07513c46d679f8fec4feb399ca27e7788
soonhankwon <soonable@gmail.com>
2023-09-15 20:24:53 +0900

    refactor : 메서드명 컨벤션에 맞게 renamed, 토큰 만료시간 static 상수로 변경(#6)

--- src/main/java/com/project5s/IDEproject/service/UserService.java
+++ src/main/java/com/project5s/IDEproject/service/UserService.java
@@ -1,14 +1,13 @@
 package com.project5s.IDEproject.service;
 
-import com.project5s.IDEproject.domain.Users;
-import com.project5s.IDEproject.domain.dto.UserResetRequest;
+import com.project5s.IDEproject.domain.User;
 import com.project5s.IDEproject.exception.AppException;
 import com.project5s.IDEproject.exception.ErrorCode;
 import com.project5s.IDEproject.repository.UserRepository;
 import com.project5s.IDEproject.utils.JwtUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -17,50 +16,49 @@ import org.springframework.transaction.annotation.Transactional;
 public class UserService {
 
     private final UserRepository userRepository;
-    private final BCryptPasswordEncoder encoder;
+    private final PasswordEncoder encoder;
 
-    @Value("${jwt.secret")
+    @Value("${jwt.secret}")
     private String key;
-    private Long expriredTimeMs = 1000 * 60 * 60l;
+    private static final Long ACCESS_TOKEN_EXPIRED_TIME_MS = 1000 * 60 * 60L;
 
-    public String join(String userName, String password) {
+    public String join(String username, String password) {
 
-        // userName 중복 check
-        userRepository.findByUserName(userName)
-                .ifPresent(users -> {throw new AppException(ErrorCode.USERNAME_DUPLICATED, userName + "은(는) 이미 존재합니다.");
+        // username 중복 check
+        userRepository.findByUsername(username)
+                .ifPresent(user -> {throw new AppException(ErrorCode.USERNAME_DUPLICATED, username + "은(는) 이미 존재합니다.");
                 });
 
         // 저장
-        Users users = Users.builder()
-                .userName(userName)
+        User user = User.builder()
+                .username(username)
                 .password(encoder.encode(password))
                 .build();
-        userRepository.save(users);
+        userRepository.save(user);
 
         return "SUCCESS";
     }
 
-    public String login(String userName, String password) {
-        //userName 없음
-        Users selectedUser = userRepository.findByUserName(userName)
-                .orElseThrow(() -> new AppException(ErrorCode.USERNAME_NOT_FOUND, userName + "이 없습니다."));
+    public String login(String username, String password) {
+        //username 없음
+        User selectedUser = userRepository.findByUsername(username)
+                .orElseThrow(() -> new AppException(ErrorCode.USERNAME_NOT_FOUND, username + "이 없습니다."));
 
         //password 틀림
         if (!encoder.matches(password, selectedUser.getPassword())) {
             throw new AppException(ErrorCode.INVALID_PASSWORD, "잘못된 패스워드입니다.");
         }
 
-        String token = JwtUtil.createToken(selectedUser.getUserName(), key, expriredTimeMs);
-
+        String token = JwtUtil.createToken(selectedUser.getUsername(), key, ACCESS_TOKEN_EXPIRED_TIME_MS);
         // 앞에서 Exception 안났으면 토큰 발행
         return token;
     }
 
     @Transactional
-    public void resetPassword(String userName, String password, String newPassword) {
-        //userName 확인
-        Users selectedUser = userRepository.findByUserName(userName)
-                .orElseThrow(() -> new AppException(ErrorCode.USERNAME_NOT_FOUND, userName + "이 없습니다."));
+    public void resetPassword(String username, String password, String newPassword) {
+        //username 확인
+        User selectedUser = userRepository.findByUsername(username)
+                .orElseThrow(() -> new AppException(ErrorCode.USERNAME_NOT_FOUND, username + "이 없습니다."));
 
         //password 확인
         if (!encoder.matches(password, selectedUser.getPassword())) {
