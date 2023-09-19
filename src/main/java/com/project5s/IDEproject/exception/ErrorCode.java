fe0256afd47d30bd1d7d5c4bd3e4819b65ed56fe
soonhankwon <soonable@gmail.com>
2023-09-19 14:44:16 +0900

    feat : ErrorCode 추가 & TODO

--- src/main/java/com/project5s/IDEproject/exception/ErrorCode.java
+++ src/main/java/com/project5s/IDEproject/exception/ErrorCode.java
@@ -7,9 +7,11 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 @Getter
 public enum ErrorCode {
+    //TODO 메세지 정의해서 final String message 로 쓰는게 좋을것 같습니다.
     USERNAME_DUPLICATED(HttpStatus.CONFLICT, ""),
     USERNAME_NOT_FOUND(HttpStatus.NOT_FOUND, ""),
-    INVALID_PASSWORD(HttpStatus.UNAUTHORIZED, "");
+    INVALID_PASSWORD(HttpStatus.UNAUTHORIZED, ""),
+    PROJECT_NOT_FOUND(HttpStatus.NOT_FOUND, "");
 
     private HttpStatus httpStatus;
     private String message;
