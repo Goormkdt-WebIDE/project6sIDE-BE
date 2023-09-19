ad9cb304cd3c56f7905bf8814e27139c401a707b
soonhankwon <soonable@gmail.com>
2023-09-19 14:47:37 +0900

    chore : git pull 시 땡겨와지지 않은 코드

--- src/main/java/com/project5s/IDEproject/config/SecurityConfig.java
+++ src/main/java/com/project5s/IDEproject/config/SecurityConfig.java
@@ -11,6 +11,8 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @RequiredArgsConstructor
@@ -40,5 +42,18 @@ public class SecurityConfig {
                 .and()
                 .addFilterBefore(new JwtFilter(secretKey), UsernamePasswordAuthenticationFilter.class)
                 .build();
+    }
+
+    // 스프링 서버 전역적으로 CORS 설정
+    @Configuration
+    public class WebConfig implements WebMvcConfigurer {
+        @Override
+        public void addCorsMappings(CorsRegistry registry) {
+            registry.addMapping("/**")
+                    .allowedOrigins("http://localhost:5173", "https://localhost:5173", "http://localhost:8080") // 허용할 출처
+                    .allowedMethods("GET", "POST") // 허용할 HTTP method
+                    .allowCredentials(true) // 쿠키 인증 요청 허용
+                    .maxAge(30000); // 원하는 시간만큼 pre-flight 리퀘스트를 캐싱
+        }
     }
 }
