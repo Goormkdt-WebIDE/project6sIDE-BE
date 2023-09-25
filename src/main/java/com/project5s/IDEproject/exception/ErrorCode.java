a52c9ee8d2166b007de5fd90fa77566b48125faf
Soonhan Kwon <113872320+soonhankwon@users.noreply.github.com>
2023-09-25 12:11:15 +0900

    [chore] compile error

--- src/main/java/com/project5s/IDEproject/exception/ErrorCode.java
+++ src/main/java/com/project5s/IDEproject/exception/ErrorCode.java
@@ -9,7 +9,7 @@ import org.springframework.http.HttpStatus;
 public enum ErrorCode {
     USERNAME_DUPLICATED(HttpStatus.CONFLICT, "중복된 이메일 혹은 중복된 이름입니다."),
     USERNAME_NOT_FOUND(HttpStatus.NOT_FOUND, ""),
-    INVALID_PASSWORD(HttpStatus.UNAUTHORIZED, "");
+    INVALID_PASSWORD(HttpStatus.UNAUTHORIZED, ""),
     PROJECT_NOT_FOUND(HttpStatus.NOT_FOUND, "");
 
     private HttpStatus httpStatus;
