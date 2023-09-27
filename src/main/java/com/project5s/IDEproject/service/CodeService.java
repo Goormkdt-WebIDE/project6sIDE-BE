92982ccecdef2549d4447e3f61bb11b3856e833b
soonhankwon <soonable@gmail.com>
2023-09-27 10:50:54 +0900

    feat : 프로젝트 최상단 코드 추가 비즈니스 로직(#18)

--- src/main/java/com/project5s/IDEproject/service/CodeService.java
+++ src/main/java/com/project5s/IDEproject/service/CodeService.java
@@ -3,8 +3,12 @@ package com.project5s.IDEproject.service;
 import com.project5s.IDEproject.controller.dto.CodeSaveReqDto;
 import com.project5s.IDEproject.domain.Code;
 import com.project5s.IDEproject.domain.Directory;
+import com.project5s.IDEproject.domain.Project;
+import com.project5s.IDEproject.exception.AppException;
+import com.project5s.IDEproject.exception.ErrorCode;
 import com.project5s.IDEproject.repository.CodeRepository;
 import com.project5s.IDEproject.repository.DirectoryRepository;
+import com.project5s.IDEproject.repository.ProjectRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -16,6 +20,7 @@ public class CodeService {
 
     private final CodeRepository codeRepository;
     private final DirectoryRepository directoryRepository;
+    private final ProjectRepository projectRepository;
 
     public void saveCodeInDirectory(String directoryId, CodeSaveReqDto dto) {
         Directory targetDirectory = directoryRepository.findById(directoryId).orElseThrow();
@@ -28,5 +33,15 @@ public class CodeService {
 
     public void deleteCode(String codeId) {
         codeRepository.deleteById(codeId);
+    }
+
+    public void saveCode(String projectId, CodeSaveReqDto dto) {
+        Project project = projectRepository.findById(projectId)
+                .orElseThrow(() -> new AppException(ErrorCode.PROJECT_NOT_FOUND, ErrorCode.PROJECT_NOT_FOUND.getMessage()));
+
+        Code code = new Code(dto);
+        codeRepository.save(code);
+        project.saveCode(code);
+        projectRepository.save(project);
     }
 }
