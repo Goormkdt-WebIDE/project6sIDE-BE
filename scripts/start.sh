bdfcd8a492ee07dc6f525266a679a476c84fe894
Shingyu Kang <rkdtlseb@naver.com>
2023-09-27 11:36:46 +0900

    [feat] copy p12 file also

--- scripts/start.sh
+++ scripts/start.sh
@@ -2,6 +2,7 @@
 
 PROJECT_ROOT="/home/ubuntu/app"
 JAR_FILE="$PROJECT_ROOT/spring-webapp.jar"
+P12_FILE="$PROJECT_ROOT/keystore.p12"
 
 APP_LOG="$PROJECT_ROOT/application.log"
 ERROR_LOG="$PROJECT_ROOT/error.log"
@@ -9,9 +10,10 @@ DEPLOY_LOG="$PROJECT_ROOT/deploy.log"
 
 TIME_NOW=$(date +%c)
 
-# build 파일 복사
+# build,p12 파일 복사
 echo "$TIME_NOW > $JAR_FILE 파일 복사" >> $DEPLOY_LOG
 cp $PROJECT_ROOT/build/libs/*.jar $JAR_FILE
+cp $PROJECT_ROOT/keystore.p12 $P12_FILE
 
 # jar 파일 실행
 echo "$TIME_NOW > $JAR_FILE 파일 실행" >> $DEPLOY_LOG
