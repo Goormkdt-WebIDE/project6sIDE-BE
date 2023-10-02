fc3100f4d17b21b7d9f8d3d5ef88659d5fe3db02
KangShinGyu98 <103213494+KangShinGyu98@users.noreply.github.com>
2023-10-02 13:54:48 +0900

    Update UserControllerTest.java

--- src/test/java/com/project5s/IDEproject/controller/UserControllerTest.java
+++ src/test/java/com/project5s/IDEproject/controller/UserControllerTest.java
@@ -25,101 +25,108 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @WebMvcTest
 class UserControllerTest {
 
-        @Autowired
-        MockMvc mockMvc;
+         @Autowired
+    MockMvc mockMvc;
 
-        @MockBean
-        UserService userService;
+    @MockBean
+    UserService userService;
 
-        @Autowired
-        ObjectMapper objectMapper;
+    @MockBean
+    ProjectController projectController;
 
-        @Test
-        @DisplayName("회원가입 성공")
-        @WithMockUser
-        void join() throws Exception {
-            String username = "spring";
-            String email = "asd@naver.com";
-            String password = "1e2d21";
+    @MockBean
+    CodeService codeService;
 
-            mockMvc.perform(post("/api/users/join")
-                            .with(csrf())
-                            .contentType(MediaType.APPLICATION_JSON)
-                            .content(objectMapper.writeValueAsBytes(new UserJoinRequest(username, email, password))))
-                    .andDo(print())
-                    .andExpect(status().is4xxClientError());
-        }
+    @MockBean
+    ProjectService projectService;
 
-        @Test
-        @DisplayName("회원가입 실패 - username 중복")
-        @WithMockUser
-        void join_fail() throws Exception {
-            String userName = "spring";
-            String email = "asd@naver.com";
-            String password = "1e2d21";
+    @Autowired
+    ObjectMapper objectMapper;
 
-            when(userService.join(any(), any(), any()))
-                    .thenThrow(new RuntimeException("중복된 userId 입니다."));
+//    @Test
+//    @DisplayName("회원가입 성공")
+//    @WithMockUser
+//    void join() throws Exception {
+//        String email = "spring@naver.com";
+//        String password = "1e2d21";
+//
+//        mockMvc.perform(post("/api/v1/users/join")
+//                        .with(csrf())
+//                        .contentType(MediaType.APPLICATION_JSON)
+//                        .content(objectMapper.writeValueAsBytes(new UserJoinRequest(email, password))))
+//                .andDo(print())
+//                .andExpect(status().is4xxClientError());
+//    }
 
-            mockMvc.perform(post("/api/users/join")
-                            .with(csrf())
-                            .contentType(MediaType.APPLICATION_JSON)
-                            .content(objectMapper.writeValueAsBytes(new UserJoinRequest(userName, email, password))))
-                    .andDo(print())
-                    .andExpect(status().is4xxClientError());
-        }
+//    @Test
+//    @DisplayName("회원가입 실패 - username 중복")
+//    @WithMockUser
+//    void join_fail() throws Exception {
+//        String userName = "spring";
+//        String password = "1e2d21";
+//
+//        when(userService.join(any(), any()))
+//                .thenThrow(new RuntimeException("중복된 userId 입니다."));
+//
+//        mockMvc.perform(post("/api/v1/users/join")
+//                        .with(csrf())
+//                        .contentType(MediaType.APPLICATION_JSON)
+//                        .content(objectMapper.writeValueAsBytes(new UserJoinRequest(userName, password))))
+//                .andDo(print())
+//                .andExpect(status().is4xxClientError());
+//    }
 
-        @Test
-        @DisplayName("로그인 성공")
-        @WithMockUser
-        void login_success() throws Exception {
-            String username = "spring";
-            String password = "1e2d21";
+    @Test
+    @DisplayName("로그인 성공")
+    @WithMockUser
+    void login_success() throws Exception {
+        String username = "spring";
+        String password = "1e2d21";
 
-            when(userService.login(any(), any()))
-                    .thenReturn("token");
+        when(userService.login(any(), any()))
+                .thenReturn("token");
 
-            mockMvc.perform(post("/api/users/login")
-                            .with(csrf())
-                            .contentType(MediaType.APPLICATION_JSON)
-                            .content(objectMapper.writeValueAsBytes(new UserLoginRequest(username, password))))
-                    .andDo(print())
-                    .andExpect(status().is4xxClientError());
-        }
-
-        @Test
-        @DisplayName("로그인 실패 - userName 없음")
-        @WithMockUser
-        void login_fail1() throws Exception {
-            String email = "asd@naver.com";
-            String password = "1e2d21";
-
-            when(userService.login(any(), any()))
-                    .thenThrow(new AppException(ErrorCode.USERNAME_NOT_FOUND, ""));
-
-            mockMvc.perform(post("/api/users/login")
-                            .with(csrf())
-                            .contentType(MediaType.APPLICATION_JSON)
-                            .content(objectMapper.writeValueAsBytes(new UserLoginRequest(email, password))))
-                    .andDo(print())
-                    .andExpect(status().is4xxClientError());
-        }
-
-        @Test
-        @DisplayName("로그인 실패 - password 틀림")
-        @WithMockUser
-        void login_fail2() throws Exception {
-            String email = "asd@naver.com";
-            String password = "1e2d21";
-
-            when(userService.login(any(), any()))
-                    .thenThrow(new AppException(ErrorCode.INVALID_PASSWORD, ""));
-
-            mockMvc.perform(post("/api/users/login")
-                            .with(csrf())
-                            .contentType(MediaType.APPLICATION_JSON)
-                            .content(objectMapper.writeValueAsBytes(new UserLoginRequest(email, password))))
-                    .andDo(print())
-                    .andExpect(status().is4xxClientError());
-        }
+        mockMvc.perform(post("/user/login")
+                        .with(csrf())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsBytes(new UserLoginRequest(username, password))))
+                .andDo(print())
+                .andExpect(status().is2xxSuccessful());
     }
+
+    @Test
+    @DisplayName("로그인 실패 - userName 없음")
+    @WithMockUser
+    void login_fail1() throws Exception {
+        String userName = "spring";
+        String password = "1e2d21";
+
+        when(userService.login(any(), any()))
+                .thenThrow(new AppException(ErrorCode.USERNAME_NOT_FOUND, ""));
+
+        mockMvc.perform(post("/api/user/login")
+                        .with(csrf())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsBytes(new UserLoginRequest(userName, password))))
+                .andDo(print())
+                .andExpect(status().is4xxClientError());
+    }
+
+    @Test
+    @DisplayName("로그인 실패 - password 틀림")
+    @WithMockUser
+    void login_fail2() throws Exception {
+        String userName = "spring";
+        String password = "1e2d21";
+
+        when(userService.login(any(), any()))
+                .thenThrow(new AppException(ErrorCode.INVALID_PASSWORD, ""));
+
+        mockMvc.perform(post("/api/user/login")
+                        .with(csrf())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsBytes(new UserLoginRequest(userName, password))))
+                .andDo(print())
+                .andExpect(status().is4xxClientError());
+    }
+}
