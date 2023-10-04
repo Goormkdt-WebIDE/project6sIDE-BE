cbf44b931de6fab687f1c8fb048c442be722c602
Shingyu Kang <rkdtlseb@naver.com>
2023-10-04 11:35:29 +0900

    [feat] email validation 추가

--- src/main/java/com/project5s/IDEproject/dto/UserLoginRequest.java
+++ src/main/java/com/project5s/IDEproject/dto/UserLoginRequest.java
@@ -1,9 +1,11 @@
 package com.project5s.IDEproject.dto;
 
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 
 public record UserLoginRequest(@NotBlank(message = "이메일은 필수 입력 값입니다.")
-                               String email,
+                               @Email(message = "이메일 형식이 올바르지 않습니다.")
+                                String email,
                                @NotBlank(message = "비밀번호는 필수 입력 값입니다.")
-                               String password) {
+                                String password) {
 }
--- src/main/java/com/project5s/IDEproject/dto/UserResetRequest.java
+++ src/main/java/com/project5s/IDEproject/dto/UserResetRequest.java
@@ -1,9 +1,11 @@
 package com.project5s.IDEproject.dto;
 
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
 
 public record UserResetRequest(@NotBlank(message = "이메일은 필수 입력 값입니다.")
+        @Email(message = "이메일 형식이 올바르지 않습니다.")
                                String email,
                                @NotBlank(message = "비밀번호는 필수 입력 값입니다.")
                                String password,
