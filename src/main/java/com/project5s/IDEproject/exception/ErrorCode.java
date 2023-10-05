775ea77d467bcfda77181de83b9100701be05aec
soonhankwon <soonable@gmail.com>
2023-10-05 21:04:53 +0900

    feat : 에러코드 추가 (#26)

--- src/main/java/com/project5s/IDEproject/exception/ErrorCode.java
+++ src/main/java/com/project5s/IDEproject/exception/ErrorCode.java
@@ -11,7 +11,10 @@ public enum ErrorCode {
     USERNAME_NOT_FOUND(HttpStatus.NOT_FOUND, ""),
     INVALID_PASSWORD(HttpStatus.UNAUTHORIZED, ""),
     PROJECT_NOT_FOUND(HttpStatus.NOT_FOUND, "프로젝트를 찾을 수 없습니다."),
-    CODE_NOT_FOUND(HttpStatus.NOT_FOUND, "코드파일을 찾을 수 없습니다.");
+    PROJECT_NOT_FOUND_BY_EMAIL(HttpStatus.NOT_FOUND, "해당 이메일 사용자의 프로젝트를 찾을 수 없습니다."),
+    CODE_NOT_FOUND(HttpStatus.NOT_FOUND, "코드파일을 찾을 수 없습니다."),
+    DIRECTORY_NOT_FOUND(HttpStatus.NOT_FOUND, "디렉토리를 찾을 수 없습니다."),
+    PROJECT_NAME_DUPLICATED(HttpStatus.CONFLICT, "이미 존재하는 프로젝트 이름입니다.");
 
     private HttpStatus httpStatus;
     private String message;
